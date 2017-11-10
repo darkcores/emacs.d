5ed8e7eb480be5fae073b81365ba17a8553f4060
Jorrit Gerets <jorrit@partybus.localdomain>
2017-11-10 20:02:29 +0100

    Switching between python and python2.

--- lang/org.el
+++ lang/org.el
@@ -4,11 +4,18 @@
 (require 'evil)
 
 ;;; Code:
-(add-hook 'org-mode-hook
-	  (lambda ()
-	    (org-bullets-mode 1)
-	    (define-key evil-normal-state-map (kbd "SPC e") 'org-export-dispatch)
-	    (define-key evil-normal-state-map (kbd "SPC '") 'org-edit-special)
-	    ))
+
+;; keybindings
+(defun org-keys-setup ()
+  "Setup SPC keybinds for org."
+  (define-key evil-normal-state-map (kbd "SPC e") 'org-export-dispatch)
+  (define-key evil-normal-state-map (kbd "SPC '") 'org-edit-special))
+
+(defun org-setup ()
+  "Setup org."
+  (org-bullets-mode 1)
+  (org-keys-setup))
+
+(add-hook 'org-mode-hook 'org-setup)
 
 ;;; org.el ends here
--- lang/python.el
+++ lang/python.el
@@ -1,6 +1,8 @@
 ;;; Python -- setup
 ;;; Documentation:
 
+(require 'flycheck)
+(require 'evil)
 (require 'anaconda-mode)
 (require 'company-anaconda)
 
@@ -9,5 +11,34 @@
 (eval-after-load "company"
  '(add-to-list 'company-backends 'company-anaconda))
 (add-hook 'python-mode-hook 'anaconda-mode)
+
+;; Python 2 support when wanted
+(defun python2-enable ()
+  "Set python2 as default python."
+  (interactive)
+  (setq flycheck-python-pycompile-executable "/usr/bin/python2")
+  (setq flycheck-python-flake8-executable "/usr/bin/flake8-python2")
+  (setq flycheck-python-pylint-executable "/usr/bin/pylint2")
+  (setq python-shell-interpreter "/usr/bin/python2")
+  (flycheck-buffer))
+
+(defun python2-disable ()
+  "Go back to default python."
+  (interactive)
+  (setq flycheck-python-pycompile-executable "/usr/bin/python")
+  (setq flycheck-python-flake8-executable "/usr/bin/flake8")
+  (setq flycheck-python-pylint-executable "/usr/bin/pylint")
+  (flycheck-buffer))
+
+;; Keybindings
+(defun python-keys-setup ()
+  "Setup python SPC keys."
+  (define-key evil-normal-state-map (kbd "SPC p e") 'python2-enable)
+  (define-key evil-normal-state-map (kbd "SPC p d") 'python2-disable)
+  (define-key evil-normal-state-map (kbd "SPC p r") 'run-python)
+  (define-key evil-normal-state-map (kbd "SPC p b") 'python-shell-send-buffer)
+  (define-key evil-normal-state-map (kbd "SPC p f") 'python-shell-send-file))
+
+(add-hook 'python-mode-hook 'python-keys-setup)
 
 ;;; python.el ends here
